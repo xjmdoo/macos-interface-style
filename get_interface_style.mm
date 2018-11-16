da6d0e071b3cab6ec71dc1b794334868105bc796
Tamas Sule <admin@tamassule.com>
2018-11-16 01:36:57 +0100

    Refactor naming

--- get_interface_style.mm
+++ get_interface_style.mm
@@ -1,14 +1,14 @@
 #import <Foundation/Foundation.h>
 #include <nan.h>
 
-void Method2(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+void GetMacOSInterfaceStyle(const Nan::FunctionCallbackInfo<v8::Value>& info) {
     NSString *interfaceStyle = [[NSUserDefaults standardUserDefaults] stringForKey:@"AppleInterfaceStyle"];
     info.GetReturnValue().Set(Nan::New([interfaceStyle UTF8String]).ToLocalChecked());
 }
 
 void Init(v8::Local<v8::Object> exports) {
   exports->Set(Nan::New("getInterfaceStyle").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Method2)->GetFunction());
+               Nan::New<v8::FunctionTemplate>(GetMacOSInterfaceStyle)->GetFunction());
 }
 
 NODE_MODULE(getInterfaceStyle, Init)
